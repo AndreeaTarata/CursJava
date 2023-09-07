545fef46a0ad13a05d4867de4c592a670c22aa14
Andreea Tarata <andreeabtarata@gmail.com>
2023-09-07 20:46:00 +0300

    curs2 intermediar

---
+++ src/Curs2/Tester.java
@@ -0,0 +1,21 @@
+package Curs2;
+
+public class Tester {
+	
+	//variabile
+	String nume;
+	int varsta;
+	int ratePerHour = 1;
+	
+	//constructori
+	//constructor default
+	//public Tester() {
+		
+	// }
+	public Tester(int salariuPeOra) {
+		ratePerHour = salariuPeOra;
+	}
+	
+	//metode
+
+}
--- src/curs1/FirstClass.java
+++ src/curs1/FirstClass.java
@@ -1,11 +1,49 @@
 package curs1;
 
+
+//one line comment
+/*
+ * multiple
+ * line
+ * coomet
+ */
+//CamelCase
+//UpperCamelCase --> clase
+//lowerCamelCase --> metode si variabile
 public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Test");
+		FirstClass obiect = new FirstClass();
+		obiect.trimiteMesaj();
+		trimiteSMS();
+	}
+	
+	public static void trimiteSMS() {
+		System.out.println("SMS");
+	}
+	public void trimiteMesaj() {
+		System.out.println("mesaj");
+		
 	}
 
 }
+
+/* 
+ * CLASA Tester() -> OBIECT1, Ob 2, Ob 3 (refolosim cod)
+ * 
+ * public class Tester(){
+ *//declarare
+ * String nume;
+ * static String prenume;
+ * 
+ * }
+ * Tester ion = new Tester();
+ * ion.prenume = "Ionut"
+ * Tester Maria = new Tester();
+ * maria.prenume - "Elena"
+ * 
+ * 
+ */
