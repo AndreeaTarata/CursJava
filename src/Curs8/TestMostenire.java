32794ee51e6f3e540d3653be0ea9cf29b13e66cc
Andreea Tarata <andreeabtarata@gmail.com>
2023-09-28 21:30:30 +0300

    curs 8 final

---
+++ src/Curs8/TestMostenire.java
@@ -0,0 +1,22 @@
+package Curs8;
+
+public class TestMostenire {
+
+	public static void main(String[] args) {
+		
+		
+		//Angajat -->  Tester -->  TesterAutomat - lant de mostenire
+		
+		TesterAutomat testerAutomat = new TesterAutomat();
+		testerAutomat.nu
+		
+		testerAutomat.setProgrmmingLanguage("Java"); // vine din clasa tester automat
+		testerAutomat.setDepartment("QA"); // vine din clasa tester
+		testerAutomat.setSeniority("Junior"); // vine din clasa tester;
+		testerAutomat.setEmail("bob@bob.com"); // vine din clasa angajat
+		testerAutomat.setNume("Bob"); // vine din clasa angajat;, testautomat e un obiect care are disponibil in fct lui alte 4 variabile pe care le poate folosi
+		
+
+	}
+
+}
