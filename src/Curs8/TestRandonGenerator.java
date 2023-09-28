7df92242d769c1ebea1049ba9d9cbbb457c1b0aa
Andreea Tarata <andreeabtarata@gmail.com>
2023-09-28 20:25:46 +0300

    curs 8 intermediar

---
+++ src/Curs8/TestRandonGenerator.java
@@ -0,0 +1,13 @@
+package Curs8;
+
+public class TestRandonGenerator {
+
+	public static void main(String[] args) {
+	
+			RandomNumberGenerator obj = new RandomNumberGenerator();
+			obj.generateNumbers();
+			obj.printArray();
+
+	}
+
+}
